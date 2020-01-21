65efa2f2d95dcb93187eb250f80382dec8a2c9e0
yrlp98 <yousef.r1999@gmail.com>
2020-01-21 22:17:32 +0330

    Create a example project

---
+++ example/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:size_config_package/size_config_package.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().int(constraints, orientation);
+            return MaterialApp(
+              title: 'SizeConfig Package Example',
+            );
+          },
+        );
+      },
+    );
+  }
+}
